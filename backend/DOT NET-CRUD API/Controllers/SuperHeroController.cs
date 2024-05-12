6ce11b2beea34fbd16237918ccd0953dcb2ae11d
Jengo7 <169521161+Jengo7@users.noreply.github.com>
2024-05-12 14:03:04 +0300

    Add model and API controller for super hero entities

---
+++ backend/DOT NET-CRUD API/Controllers/SuperHeroController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOT_NET_CRUD_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuperHeroController : ControllerBase
+    {
+        
+    }
+}
