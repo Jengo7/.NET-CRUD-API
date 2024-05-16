037484d4a71d1b94a49424f5fe0f6b0cb8b61074
Jengo7 <169521161+Jengo7@users.noreply.github.com>
2024-05-16 23:14:13 +0300

    Update model and API controller for super hero entities

--- backend/DOT NET-CRUD API/Controllers/SuperHeroController.cs
+++ backend/DOT NET-CRUD API/Controllers/SuperHeroController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DOT_NET_CRUD_API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DOT_NET_CRUD_API.Controllers
@@ -10,6 +11,22 @@ namespace DOT_NET_CRUD_API.Controllers
     [Route("api/[controller]")]
     public class SuperHeroController : ControllerBase
     {
-        
+        [HttpGet]
+
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero> {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+
+            return Ok(heroes);
+        }
     }
 }
